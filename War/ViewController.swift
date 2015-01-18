27797c577511bda6adb9657b146f1ed9feb048c5
Aman Agrawal <AmanAgrawal@0587364557.wireless.umich.net>
2015-01-17 23:22:13 -0500

    Initial Commit

---
+++ War/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Aman Agrawal on 1/17/15.
+//  Copyright (c) 2015 Aman Agrawal Industries Corporation Ltd. Std. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
